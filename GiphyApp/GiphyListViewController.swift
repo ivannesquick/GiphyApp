167cb391b0e00b077cc1b071ecbafe7d34b80a80
Ivan Neskin <in@decentury.io>
2023-01-28 14:22:52 +0700

    initial commit

--- GiphyApp/GiphyListViewController.swift
+++ GiphyApp/GiphyListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GiphyListViewController.swift
 //  GiphyApp
 //
 //  Created by Иван Нескин on 28.01.2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GiphyListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
